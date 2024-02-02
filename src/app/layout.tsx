ec3ba93ffca1d56527e65dec536957448e7f441e
Vignesh <vigneshpamu2002@gmail.com>
2024-02-02 17:19:26 +0530

    sdfasdfsdf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { DM_Sans } from 'next/font/google'
+import { twMerge } from 'tailwind-merge'
 import db from '@/lib/supabase/db'
 import { ThemeProvider } from '@/lib/providers/next-theme-provider'
-const inter = Inter({ subsets: ['latin'] })
+
+const inter = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,7 +21,7 @@ export default function RootLayout({
   console.log(db)
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={twMerge('bg-background', inter.className)}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
         </ThemeProvider>
